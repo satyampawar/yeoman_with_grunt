bfeef97092ba356397d365b0f30698d5046b6495
Satyam Pawar <satyamyuvasoft158@gmail.com>
2017-02-16 11:09:04 +0530

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express =  require("express")
 var app = express();
 app.use(express.static(__dirname))
-var port =  process.env.PORT || 3000;
+var port =  process.env.PORT || 9000;
 app.listen(port)
 console.log("listen")
