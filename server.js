64c2ec8ed93057f0c6b5b147a166adcd8b100e7a
Satyam Pawar <satyamyuvasoft158@gmail.com>
2017-02-16 11:33:41 +0530

    fixed server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express =  require("express")
 var app = express();
-app.use(express.static(__dirname))
+app.use(express.static(__dirname+'/app'))
+
+// app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.redirect('/views/bank_accounts/index.html');
+});
+
+
 var port =  process.env.PORT || 9000;
+// require('./app/scripts/angular.js')(app);
+// require('./app/scripts/angular-route.js')(app);
+// require('./app/scripts/angular-resource.js')(app);
+// require('./app/index.html')(app);
+
 app.listen(port)
 console.log("listen")
